bf4de4a4b4afb4811eacdeba48f37033112ef6ea
Tony Sorensen <tony48853@gmail.com>
2020-05-25 19:18:40 -0700

    Tasks 1-2 complete

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-
-
-
-
+let principal = 200000;
+let interestRate= 0.05;
+let years = 30;
 
 // 🏡 Task 1.5: Simple Math
 /* To create a monthly mortgage rate calculator, we need to know the number of years in months and the monthly interest rate. 
@@ -15,7 +14,8 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 
 Create another variable called `periods` and give it the value of years*12.
 */
-
+let monthlyInterestRate = interestRate/12;
+let periods = years*12;
 
 
 
@@ -28,8 +28,9 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
-
-
+let numerator = monthlyInterestRate * Math.pow(( 1 + monthlyInterestRate),(periods));
+let denominator= Math.pow((1 + monthlyInterestRate),(periods))-1;
+let monthlyRate = principal * (numerator/denominator);
 
 
 // 🏡 Task 3: Function
